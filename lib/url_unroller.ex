d03b9b8a5e9e77582cf69422550e493e892565cd
Jeffrey Chupp <jeff@semanticart.com>
2015-01-06 21:39:48 -0500

    add basic implementation

--- lib/url_unroller.ex
+++ lib/url_unroller.ex
@@ -1,5 +1,32 @@
 defmodule UrlUnroller do
+  @user_agent [ {"User-agent", "UrlUnroller jeff@semanticart.com"} ]
+
   def unroll(url) do
-    # pending
+    fetch(HTTPoison.head(url, @user_agent), url)
+  end
+
+  defp fetch({:ok, response}, url) do
+    handle(normalize_status_code(response), url, response)
+  end
+  defp fetch({status, response}, url) do
+    {:error, url, {status, response}}
+  end
+
+  defp handle(300, _, response) do
+    unroll(location(response))
+  end
+  defp handle(200, url, _) do
+    {:ok, url}
+  end
+  defp handle(normalized_status_code, url, response) when normalized_status_code == 400 or normalized_status_code == 500 do
+    {:error, url, {:ok, response}}
+  end
+
+  defp location(response) do
+    response.headers["Location"]
+  end
+
+  defp normalize_status_code(response) do
+    div(response.status_code, 100) * 100
   end
 end
--- test/url_unroller_test.exs
+++ test/url_unroller_test.exs
@@ -3,12 +3,15 @@ defmodule UrlUnrollerTest do
 
   import UrlUnroller
 
-  test "unrolling a shortened link" do
+  test "unrolling a bit.ly link" do
     assert unroll("http://bit.ly/1Bx0zo8") == {:ok, "http://blog.semanticart.com/"}
   end
 
   test "unrolling an invalid link" do
-    { status, _ } = unroll("http://bit.ly/fdskljfldksjfdsklj")
-    assert status == :error
+    {status, url, response_info} = unroll("http://bit.ly/fdskljfldksjfdsklj")
+    assert {status, url} == {:error, "http://bit.ly/fdskljfldksjfdsklj"}
+
+    {_, response} = response_info
+    assert response.status_code == 404
   end
 end
